b1a585d08ba9e40677c7cd20c38126a233a52d05
sebfoe <64479644+sebfoe@users.noreply.github.com>
2020-06-04 12:17:06 +0200

    Update find.sh

--- shell/find.sh
+++ shell/find.sh
@@ -11,7 +11,7 @@ $ find /path/to/data -name '*S2A*' -name '*T32*'
 # using masked and space-separated brackets
 $ find \( -name '*0730*' -o -name '*0704*' \) -name '*.SAFE*'
 ###################################################################
-# and saving this to a file using a shell script
+# and saving this to a file using a shell
 # without using /path/to/data only gives the filename without subdirectories
 # and save it to the given path, leaving it emtpy uses /path/to/dir
 #   - shebang (#!/bin/<interpreter>)
@@ -29,14 +29,35 @@ echo $list > /given/path/file.txt
 #!/bin/bash
 list="$(find /path/to/dir \( -name '*0730*' -o -name '*0704*' \) -name '*.SAFE*')"
 echo "$list" > /given/path/file.txt
-
+###################################################################
 # in one command to separate lines in file
 echo "$(find /path/to/dir \( -name '*0730*' -o -name '*0704*' \) -name '*.SAFE*')" > /given/path/file.txt
 
 # shell command scp (secure copy), possible alternative could be 'rsync'
 # copies "file" from "remote" to working dir
-$ scp remote@ip_remote:file
-# copies dir (-r, recursive) to working directory using a key (-i) for the login into the remote machine
+$ scp remote@ip_remote:file /wd
+# copies dir (-r, recursive) to working directory using a key (-i)
 scp -i /path/to/key.txt -r remote@ip_remote:/wanted/dir
-# copies directories from a list to local working directory NOT TESTED!
-cp -r user@server:/location/{"$(cat folder_list.txt | tr '\n' ',' | sed 's/,$//')"}
+# when using a key (-i) a possible solution for automate copying
+# reading txt-file to list
+list="$(<filename.txt)"
+# make array
+array=($list)
+# list first element
+echo ${array[0]}
+####################################################################
+# combined to a script
+ #!/bin/bash
+ 
+list="$(</home/UNI-WUERZBURG.EU/sef35id/Desktop/tilesearch/2019/T32UPA.txt)"
+array=($list) # not sure if needed
+#echo ${array[0]}
+
+for i in {x..y}
+do
+	echo "Downloading: ${array[i]}"
+	scp -i /path/to/key.txt -r remote@ip_remote:${array[i]} /destination
+done
+#######################################################################
+
+
