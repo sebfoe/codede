b25f2c5c4b6a000b5b6efbd439a58b3dba9b9525
sebfoe <64479644+sebfoe@users.noreply.github.com>
2020-06-25 11:51:27 +0200

    Update code-de_api.sh

--- shell/code-de_api.sh
+++ shell/code-de_api.sh
@@ -7,5 +7,6 @@
 
 
 # getting human-readable (_pretty=true) download links. e.g 10 records (maxRecords)
-# TODO cut to proper link
-wget -O - "https://finder.code-de.org/resto/api/collections/Sentinel2/search.json?_pretty=true&maxRecords=10" | grep "url"
+# 
+wget -O - "https://finder.code-de.org/resto/api/collections/Sentinel2/search.json?_pretty=true&maxRecords=10" | grep "url" | egrep -o 'h[^"]+' | sed 's/\\//g'
+
